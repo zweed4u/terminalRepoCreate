3816bc9f2464f919cbe4a7d2c0cb460d0fe8a6b4
zweed4u <zackweeden@aol.com>
2015-07-22 15:43:36 -0400

    Implemented creation of local repo

--- repoCreate.sh
+++ repoCreate.sh
@@ -11,6 +11,10 @@ echo Repository name?
 read repo_Name
 echo -e '\n'
 
+mkdir ~/Desktop/$repo_Name
+
 
 curl -u "$Username:$Token" https://api.github.com/user/repos -d '{"name":"'$repo_Name'"}'
+
+echo '\n\nLocal repository folder created at: ~/Desktop/$repo_Name'
 
